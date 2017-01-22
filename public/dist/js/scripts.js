ae83aca79c2ebe55dec2e0066e26986ce34b327b
RachelRomee <rachelromee@icloud.com>
2017-01-22 14:34:27 +0100

    test

--- public/dist/js/scripts.js
+++ public/dist/js/scripts.js
@@ -19,14 +19,14 @@ var url = "https://v2.decipherinc.com/api/v1/surveys/53c/161202/data?format=json
 //
 // }
 
-d3.request(url)
-	.header("Accept-Language", "en-US")
-	.header("X-Requested-With", "XMLHttpRequest")
-	.header("x-apikey", "spheymbxvegyh0dydw9pgc2hfhx69sn7q1rfth1zcs2dpbdu1mfru5q4qkrp7e8v")
-	.user("rachel.van.der.laan@epiphany-rbc.com")
-	.password("prinsengrachT247")
-	.mimeType("application/json")
-	.response(function(xhr) { return JSON.parse(xhr.responseText); });
+// d3.request(url)
+// 	.header("Accept-Language", "en-US")
+// 	.header("X-Requested-With", "XMLHttpRequest")
+// 	.header("x-apikey", "spheymbxvegyh0dydw9pgc2hfhx69sn7q1rfth1zcs2dpbdu1mfru5q4qkrp7e8v")
+// 	.user("rachel.van.der.laan@epiphany-rbc.com")
+// 	.password("prinsengrachT247")
+// 	.mimeType("application/json")
+// 	.response(function(xhr) { return JSON.parse(xhr.responseText); });
 //
 
 
@@ -47,7 +47,7 @@ d3.json(url)
 	.header("x-apikey", "spheymbxvegyh0dydw9pgc2hfhx69sn7q1rfth1zcs2dpbdu1mfru5q4qkrp7e8v")
 	.user("rachel.van.der.laan@epiphany-rbc.com")
 	.password("prinsengrachT247")
-	.mimeType("application/json")
+	// .mimeType("application/json")
   	.get(function(error, data) {
 		{ return JSON.parse(data.responseText); }
 		console.log('d3.json' + data)
