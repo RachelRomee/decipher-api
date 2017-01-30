a8dc982af5d9f3905bde339a6632b315304bab76
RachelRomee <rachelromee@icloud.com>
2017-01-30 11:29:31 +0100

    api test

--- public/js/charts/linechart.js
+++ public/js/charts/linechart.js
@@ -1,7 +1,7 @@
 // https://www.google.nl/url?sa=t&rct=j&q=&esrc=s&source=web&cd=13&cad=rja&uact=8&ved=0ahUKEwjS9pa76rHRAhVGthoKHUkzAU04ChAWCDMwAg&url=https%3A%2F%2Fwww.npmjs.com%2Fpackage%2Fd3-request&usg=AFQjCNEvDqTuSK7s4VUaYuK3fCy6x3DFfg&sig2=DiqJuFKXmrytJ-d08XKOzQ
 
 var url = "https://v2.decipherinc.com/api/v1/surveys/53c/161202/data?format=json";
-//
+
 // d3.json(url, function(data) {
 // 	.header("Accept-Language", "en-US")
 // 	.header("X-Requested-With", "XMLHttpRequest")
@@ -9,15 +9,8 @@ var url = "https://v2.decipherinc.com/api/v1/surveys/53c/161202/data?format=json
 // 	.user("rachel.van.der.laan@epiphany-rbc.com")
 // 	.password("prinsengrachT247")
 // 	.mimeType("application/json")
-//
-// 	console.log(data);
-//
-// 	// test
 // });
 
-// function getData() {
-//
-// }
 
 // d3.request(url)
 // 	.header("Accept-Language", "en-US")
@@ -30,32 +23,20 @@ var url = "https://v2.decipherinc.com/api/v1/surveys/53c/161202/data?format=json
 //
 
 
-
-// d3.json("http://something.that.returns.json",function(error,response){
-// //TASK 1
-// //process http call here.
-//
-// //TASK 2
-// //render the graph here
-// });
-
-request.header("x-apikey", "spheymbxvegyh0dydw9pgc2hfhx69sn7q1rfth1zcs2dpbdu1mfru5q4qkrp7e8v")
+// request.header("x-apikey", "spheymbxvegyh0dydw9pgc2hfhx69sn7q1rfth1zcs2dpbdu1mfru5q4qkrp7e8v")
 
 // You can add request headers with the #header method:
 // https://github.com/mbostock/d3/wiki/Requests#header
 d3.json(url)
+    .header("x-apikey", "spheymbxvegyh0dydw9pgc2hfhx69sn7q1rfth1zcs2dpbdu1mfru5q4qkrp7e8v")
+    .user("rachel.van.der.laan@epiphany-rbc.com")
+    .password("prinsengrachT247")
   	.get(function(error, data) {
-		{ return JSON.parse(data.responseText); }
+		 return JSON.parse(data.responseText);
   })
 //
 
-
-// d3.json(url, function(data) {
-// 	console.log(data)
-// })
-
 // request.get([data][, callback])
-
 // request.user([value])
 // request.password([value])
 
@@ -64,8 +45,5 @@ d3.json(url)
 //     .response(function(xhr) { return JSON.parse(xhr.responseText); })
 //     .get(callback);
 
-
-
-
-	// header "x-apikey: API_KEY_GOES_HERE
-	// spheymbxvegyh0dydw9pgc2hfhx69sn7hja5y719n2a2hysb7p7x2z1728yfz6u1
+// header "x-apikey: API_KEY_GOES_HERE
+// spheymbxvegyh0dydw9pgc2hfhx69sn7hja5y719n2a2hysb7p7x2z1728yfz6u1
